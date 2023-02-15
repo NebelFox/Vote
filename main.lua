a63d937793b508807a8de959c2ec5322af2ac23f
nebelfox <mashtaliaryaroslav@gmail.com>
2023-02-15 22:43:08 +0200

    Add cross-scenes clickable credits

--- main.lua
+++ main.lua
@@ -11,4 +11,11 @@ local composer = require("composer")
 local widgets = require 'src.widgets'
 widgets.init()
 
+widgets.Credits({
+    parent = display.getCurrentStage(),
+    text = 'made by @nebelfox',
+    url = 'https://github.com/NebelFox/Vote#readme',
+    alpha = 0.2
+})
+
 composer.gotoScene 'src.scenes.setup'
